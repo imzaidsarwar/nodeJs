c5733d6381a3d2350644b93f1d97eea50520a556
Mohd Zaid Sarwar <140310771+imzaidsarwar@users.noreply.github.com>
2023-07-30 16:47:02 +0530

    Add files via upload

---
+++ intro-to-nodejs/index.js
@@ -0,0 +1,10 @@
+//console.log("Hello Zaid");
+//jshint esverion:6 
+//const fs = require("fs");
+///fs.copyFileSync("index.txt","file1.txt");
+
+const superheroes = require('superheroes');
+
+var mySuperHero= superheroes.random();
+
+console.log(mySuperHero);
